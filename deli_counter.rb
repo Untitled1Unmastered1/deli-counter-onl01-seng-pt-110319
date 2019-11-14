ffb9e90b80f71e4644ec99e1bf8ffb5ea121dde8
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-14 06:48:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
-def line(array)
-  if array.size == 0
+def line(thom)
+  if thom.size == 0
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-    array.each_with_index do |name, i|
+    thom.each_with_index do |name, i|
      current_line << " #{i + 1}. #{name}"
     end
       puts current_line
